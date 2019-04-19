3ac796a67d08b700391bd1b563db47f2029f4771
Romane G <romane71193@gmail.com>
2019-04-18 20:57:58 -0400

    wrapped label around search

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,12 +3,13 @@ import React from 'react';
 const SearchBox = ({ searchfield, searchChange }) => {
   return (
     <div className='pa2'>
+     <label for="search">
       <input
         className='pa3 ba b--green bg-lightest-blue'
         type='search'
         placeholder='search robots'
         onChange={searchChange}
-      />
+      /></label>
     </div>
   );
 }
